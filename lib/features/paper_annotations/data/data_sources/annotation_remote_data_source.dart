044e7aaa58d5e1c12026912a44f90df85124bbaf
Amr Nabih <amrnabih112@gmail.com>
2026-04-28 21:16:59 +0300

    feat: Adding the remote datasourcd of Annotation and connect it to the presentation layer

--- lib/features/paper_annotations/data/data_sources/annotation_remote_data_source.dart
+++ lib/features/paper_annotations/data/data_sources/annotation_remote_data_source.dart
@@ -3,7 +3,18 @@ import '../models/highlight_model.dart';
 /// Remote data source for annotations (Backend API)
 abstract class AnnotationRemoteDataSource {
   /// Get all highlights for a paper from the server
-  Future<List<HighlightModel>> getHighlights(String paperId);
+  Future<List<HighlightModel>> getHighlights(
+    String paperId, {
+    int page,
+    int limit,
+  });
+
+  /// Get all highlights with notes for a paper from the server
+  Future<List<HighlightModel>> getAnnotatedHighlights(
+    String paperId, {
+    int page,
+    int limit,
+  });
 
   /// Save a highlight to the server
   Future<HighlightModel> saveHighlight(HighlightModel highlight);
@@ -12,5 +23,25 @@ abstract class AnnotationRemoteDataSource {
   Future<HighlightModel> updateHighlight(HighlightModel highlight);
 
   /// Delete a highlight from the server
-  Future<void> deleteHighlight(String highlightId);
+  Future<void> deleteHighlight(String paperId, String highlightId);
+
+  /// Add a note to an existing highlight
+  Future<HighlightModel> addHighlightNote({
+    required String paperId,
+    required String highlightId,
+    required String note,
+  });
+
+  /// Update a note on an existing highlight
+  Future<HighlightModel> updateHighlightNote({
+    required String paperId,
+    required String highlightId,
+    required String note,
+  });
+
+  /// Delete a note from an existing highlight
+  Future<void> deleteHighlightNote({
+    required String paperId,
+    required String highlightId,
+  });
 }
---
+++ lib/features/paper_annotations/domain/usecases/delete_highlight_note_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:mirath/core/error/failuors.dart';
+import 'package:mirath/core/usecases/usecase.dart';
+import '../entites/highlight_note_params.dart';
+import '../repositories/annotation_repository.dart';
+
+class DeleteHighlightNoteUseCase implements UseCase<void, HighlightNoteParams> {
+  final AnnotationRepository repository;
+
+  DeleteHighlightNoteUseCase(this.repository);
+
+  @override
+  Future<Either<Failure, void>> call(HighlightNoteParams params) async {
+    return await repository.deleteHighlightNote(params);
+  }
+}
