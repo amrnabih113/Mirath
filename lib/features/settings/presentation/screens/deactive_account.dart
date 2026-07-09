b3f8e9d770ca62e4617f4c5c68a79e1e7fb05d87
Noura Ebrahim Siam <157627145+NouraEbrahim010@users.noreply.github.com>
2026-07-09 22:45:15 +0300

    setting finish

--- lib/features/settings/presentation/screens/deactive_account.dart
+++ lib/features/settings/presentation/screens/deactive_account.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:mirath/core/constants/route_names.dart';
 import 'package:mirath/core/ui/widgets/my_app_bar.dart';
 import 'package:mirath/core/utils/my_colors.dart';
 import 'package:mirath/core/utils/my_extenstions.dart';
